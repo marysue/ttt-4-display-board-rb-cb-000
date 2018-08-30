61d04854d30a78705f2252b36fd334445760dd4d
Mary Lark <mary@marylark.com>
2018-08-30 18:15:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,17 @@
-board = ["X", "O", "X", "X", "O", "X", "X", "O", "X"]# 
+board = ["X", "O", "X", "X", "O", "X", "X", "O", "X"]#
 
 def display_board(barray)
-  # X |   |   "
+  #"   |   |   "
+  #"-----------"
+  #"   |   |   "
+  #"-----------"
+  #"   |   |   "
   puts("#{barray[0]}", " | ", "#{barray[1]}", " | ", "#{barray[2]}")
   puts("___________")
   puts("#{barray[3]}", " | ", "#{barray[4]}", " | ", "#{barray[5]}")
   puts("___________")
   puts(barray[6], " | ", barray[7], " | ", barray[8])
-  
+
 end
 
 display_board(board)
-
-
